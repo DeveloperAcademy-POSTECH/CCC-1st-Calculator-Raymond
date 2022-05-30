69e967fecf33d1d5502ec7195a1a329acfa24e16
SH0123 <sanghyoboy@naver.com>
2022-05-30 19:44:18 +0900

    before operation

--- calculator_clone/calculator_clone/ViewController.swift
+++ calculator_clone/calculator_clone/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var valueDisplay: UILabel!
     @IBOutlet weak var resetButton: UIButton!
@@ -16,33 +16,33 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var plusButton: UIButton!
     
-    var prevValue: String = "0"
-    var nextValue: String = ""
-    var commaValue: String = "0"
-    var labelTextSize: CGFloat = 90
-    var isPositive: Bool = true
-    var operationSymbol: OperationSymbol?
-    var rightBeforeSymbpl: OperationSymbol?
+    private var nowValue: String = "0"
+    private var prevValue: Float = ""
+    private var commaValue: String = "0"
+    private var labelTextSize: CGFloat = 90
+    private var isPositive: Bool = true
+    private var operationSymbol: OperationSymbol?
+    private var rightBeforeSymbpl: OperationSymbol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        }
     
     //코드 지저분함 개선 필요(toggleSign에서부터 지저분해짐)
     @IBAction func tapNumber(_ sender: UIButton) {
         let buttonText = sender.titleLabel?.text
         //연산(=, +, - ..) 이후 입력하면 새로운 값 입력되도록
-        if prevValue == "0" || prevValue == "-0"{
-            prevValue = buttonText!
-            if !isPositive{ prevValue.insert("-", at: prevValue.startIndex)}
-            setComma()
+        if nowValue == "0" || nowValue == "-0"{
+            nowValue = buttonText!
+            if !isPositive{ nowValue.insert("-", at: nowValue.startIndex)}
+            setComma(nowValue)
             valueDisplay.text = commaValue
             resetButton.titleLabel?.text = "C"
         }
-        else if prevValue.getIntDigit() <= 8{
-            prevValue += buttonText!
-            setComma()
+        else if nowValue.getIntDigit() <= 8{
+            nowValue += buttonText!
+            setComma(nowValue)
             valueDisplay.text = commaValue
             resizeLabelText()
             valueDisplay.font = valueDisplay.font.withSize(labelTextSize)
@@ -50,7 +50,8 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func resetValue(_ sender: UIButton) {
-        prevValue = "0"
+        print(sender.state)
+        nowValue = "0"
         commaValue = "0"
         isPositive = true
         labelTextSize = 90
@@ -61,26 +62,26 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func setDot(_ sender: UIButton) {
         //사칙연산 부호 이후 . 눌렀을 대 0.으로 뜨는 처리
-        if prevValue.getIntDigit() < 9  && prevValue.firstIndex(of:".") == nil{
-            prevValue += "."
-            setComma()
+        if nowValue.getIntDigit() < 9  && nowValue.firstIndex(of:".") == nil{
+            nowValue += "."
+            setComma(nowValue)
             valueDisplay.text = commaValue
         }
     }
     
     @IBAction func toggleSign(_ sender: UIButton) {
-        let firstIndex = prevValue.startIndex
+        let firstIndex = nowValue.startIndex
 
         if isPositive{
             isPositive = false
-            labelTextSize = prevValue.getIntDigit() >= 6 ? labelTextSize - 12 : labelTextSize
-            prevValue.insert("-", at: firstIndex)
+            labelTextSize = nowValue.getIntDigit() >= 6 ? labelTextSize - 12 : labelTextSize
+            nowValue.insert("-", at: firstIndex)
         }else{
             isPositive = true
-            prevValue.remove(at: firstIndex)
-            labelTextSize = prevValue.getIntDigit() >= 6 ? labelTextSize + 12 : labelTextSize
+            nowValue.remove(at: firstIndex)
+            labelTextSize = nowValue.getIntDigit() >= 6 ? labelTextSize + 12 : labelTextSize
         }
-        setComma()
+        setComma(nowValue)
         valueDisplay.text = commaValue
         valueDisplay.font = valueDisplay.font.withSize(labelTextSize)
     }
@@ -101,8 +102,8 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func percentFunction(_ sender: UIButton) {
-        prevValue = prevValue != "0" ? String(Double(prevValue)! / 100) : prevValue
-        setComma()
+        nowValue = nowValue != "0" ? String(Float(nowValue)! / 100) : nowValue
+        setComma(nowValue)
         valueDisplay.text = commaValue
     }
     
@@ -110,9 +111,25 @@ class CalculatorViewController: UIViewController {
         
     }
     
+    func calculateDivide(){
+        prevValue /= Float(nowValue)!
+    }
+    
+    func calculateMultiply(){
+        prevValue *= Float(nowValue)!
+    }
+    
+    func calculateMinus(){
+        prevValue -= Float(nowValue)!
+    }
+    
+    func calculatePlus(){
+        prevValue += Float(nowValue)!
+    }
+    
     //minus, comma, dot을 숫자로 인지하지 않도록 하는 코드가 다소 복잡해보임, 간결화 필요
-    func setComma(){
-        commaValue = prevValue
+    func setComma(_ beforeSetComma: String){
+        commaValue = beforeSetComma
         var intCount = commaValue.getIntDigit()
         var lastIndex = commaValue.endIndex
         
@@ -134,27 +151,20 @@ class CalculatorViewController: UIViewController {
         }
     }
     
-    //자연스러운 크기 감소 반영 필요
+    //자연스러운 크기 감소 반영 필요, -부호 있을 때 7번째 자리수부터 ...발생, 사이즈 조절 필요
     func resizeLabelText(){
-        switch prevValue.getIntDigit(){
+        switch nowValue.getIntDigit(){
         case 7:
-            labelTextSize -= 12
+            labelTextSize -= 13
         case 8:
-            labelTextSize -= 12
+            labelTextSize -= 10
         case 9:
-            labelTextSize -= 12
+            labelTextSize -= 7
         case 10:
-            labelTextSize -= 12
+            labelTextSize -= 7
         default:
             break
         }
-    }
-    
-    func roundValue(_ targetValue: Double) -> String{
-        let digit: Double = pow(10, 8)
-        let res = round(targetValue * digit) / digit
-        
-        return String(res)
     }
 }
 
